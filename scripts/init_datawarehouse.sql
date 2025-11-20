9532d9993b25ed3eaba7e84c065bb9dd8fff540b
igwefrancis <igwefrancisnchedo@gmail.com>
2025-11-20 12:13:24 +0000

    Add files via upload

---
+++ scripts/init_datawarehouse.sql
@@ -0,0 +1,8 @@
+/* This scrip creates  database with the name datawarehouse
+and the schemas bronze,silver and gold.
+
+create database datawarehouse;
+use datawarehouse;
+create schema bronze;
+create schema silver;
+create schema gold;
